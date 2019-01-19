b9149e9d6e79d21e858e74eb38be07db3cd989e1
SarcasticGeek <mohamedessamfathalla@gmail.com>
2019-01-19 19:50:16 +0200

    add controllers with auth middleware

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    function index() {
+        return view('home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', 'ArticleController@index')->name('home');
+});
